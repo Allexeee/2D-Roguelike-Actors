443b3118442945ec6a79547a2162ebc027b31b79
Alexeee <aleksey_ohezin@mail.ru>
2019-04-11 22:46:10 +0500

    Обновление фреймворка до 07.04.2019 с фиксами от 11.04.2019

---
+++ Assets/Source/Runtime/GameSession.cs
@@ -0,0 +1,13 @@
+﻿//   Project : Actors
+//  Contacts : Pixeye - ask@pixeye.games 
+
+using UnityEngine;
+
+namespace Pixeye.Framework
+{
+    [CreateAssetMenu(fileName = "GameSession", menuName = "Actors Framework/Add/Data/GameSession")]
+    public class GameSession : DataGame, IKernel
+    {
+        public static GameSession Default => Toolbox.Get<GameSession>();
+    }
+}
---
+++ Assets/Source/Runtime/GameSettings.cs
@@ -0,0 +1,15 @@
+﻿//   Project : Actors
+//  Contacts : Pixeye - ask@pixeye.games 
+
+using UnityEngine;
+
+namespace Pixeye.Framework
+{
+	[CreateAssetMenu(fileName = "GameSettings", menuName = "Actors Framework/Add/Data/GameSettings")]
+	public class GameSettings : DataGame, IKernel
+	{
+
+		public static GameSettings Default => Toolbox.Get<GameSettings>();
+
+	}
+}
